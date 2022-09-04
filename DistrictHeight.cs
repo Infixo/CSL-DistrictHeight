8c216d3f0b56ed495bb866ed9fc62080b11345b4
Infixo <swidwins@gmail.com>
2022-09-04 02:59:40 +0200

    Testing hardcoded stuff

---
+++ DistrictHeight.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+//using System.Collections.Generic;
+using System.Linq;
+//using System.Text;
+//using ColossalFramework;
+//using UnityEngine;
+
+namespace DistrictHeight
+{
+    public static class BuildingInfo_Extensions
+    {
+        public static float GetHeight(this BuildingInfo instance)
+        {
+            // height - snippet from FindIt
+            if (instance.m_generatedInfo?.m_heights != null)
+                if (instance.m_generatedInfo.m_heights.Length > 0)
+                    return instance.m_generatedInfo.m_heights.Max();
+            return 0f;
+        }
+    }
+} // namespace
---
+++ DistrictHeightMod.cs
@@ -0,0 +1,46 @@
+﻿using CitiesHarmony.API;
+using ICities;
+
+namespace DistrictHeight
+{
+    public class DistrictHeightMod : IUserMod, ILoadingExtension
+    {
+        public string Name => "District Height";
+        public string Description => "Controls height of buildings in the distric";
+
+        public void OnEnabled()
+        {
+            HarmonyHelper.DoOnHarmonyReady(() => DistrictHeightPatcher.PatchAll());
+        }
+
+        public void OnDisabled()
+        {
+            if (HarmonyHelper.IsHarmonyInstalled) DistrictHeightPatcher.UnpatchAll();
+        }
+
+        // called when level loading begins
+        public void OnCreated(ILoading loading)
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        // called when level is loaded
+        public void OnLevelLoaded(LoadMode mode)
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        // called when unloading begins
+        public void OnLevelUnloading()
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        // called when unloading finished
+        public void OnReleased()
+        {
+            //throw new System.NotImplementedException();
+        }
+
+    }
+}
